eed64d9ea0bd4db70353b8cc90043125a02887bb
vg8714-oss <vg8714@srmist.edu.in>
2026-04-21 01:57:27 +0530

    Use case 1 added

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,14 @@
+/*
+This is Develop branch
+Use Case 1
+
+@version 1.1
+
+*/
+
 class HelloApp{
 	public static void main(String[] args){
 		System.out.println("Hello App");
 		
-		System.out.println("Develop Branch");
 	}
 }
